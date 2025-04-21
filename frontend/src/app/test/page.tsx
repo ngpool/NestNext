ea823999a480c4d680bb46081610e5bd8970ef26
N型 <u11b146h@gmail.com>
2025-04-21 21:19:01 +0900

    雷エフェクト

--- frontend/src/app/test/page.tsx
+++ frontend/src/app/test/page.tsx
@@ -1,7 +1,17 @@
-export default function Front() {
+"use client";
+
+import Lightning from './components/lightning';
+
+export default function TestPage() {
   return (
-    <div>
-      <h1>test</h1>
+    <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+      <Lightning
+        hue={220}
+        xOffset={0}
+        speed={1}
+        intensity={1}
+        size={1}
+      />
     </div>
   );
 }
